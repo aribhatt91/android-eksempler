4db4a2b16634d244edeb02a2d4fb7e64a19ca07a
jacob.nordfalk <jacob.nordfalk@5618ce10-0a77-33da-ee7f-8326ecd09c32>
2011-04-19 15:37:30 +0000

    Import af eksempelsamling fra http://code.google.com/p/simpla/

---
+++ trunk/AndroidElementer/src/eks/diverse/BenytWebView.java
@@ -0,0 +1,36 @@
+package eks.diverse;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class BenytWebView extends Activity {
+	
+	public class MinKlasse {
+		public void visToast(String tekst) {
+			Toast.makeText(BenytWebView.this, tekst, Toast.LENGTH_LONG).show();
+		}
+		public void afslut() {
+			finish();
+		}
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		WebView webView = new WebView(this);
+	    //webView.loadUrl("http://javabog.dk");
+	    webView.loadUrl("file:///android_asset/benytwebview.html");	    
+
+	    webView.getSettings().setJavaScriptEnabled(true);
+	    //webView.loadUrl("javascript:alert('Hej')");
+	    
+	    MinKlasse mitObjekt = new MinKlasse();
+	    mitObjekt.visToast("Toast fra Java");
+	    webView.addJavascriptInterface(mitObjekt, "mitObjekt");
+
+	    
+	    setContentView(webView);
+	}
+}
