eb27b8c4e80c8c3cb51f91218f7469b0c561fb03
jacob.nordfalk <jacob.nordfalk@5618ce10-0a77-33da-ee7f-8326ecd09c32>
2011-05-24 10:06:02 +0000

    udgiver v 1.09 på Marked

---
+++ AndroidElementer/assets/src/eks/grafik/FlytbarVejrudsigt.java
@@ -0,0 +1,103 @@
+package eks.grafik;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import eks.vejret.ByvejrAktivitet;
+
+/**
+ *
+ * @author Jacob Nordfalk
+ */
+public class FlytbarVejrudsigt extends Activity {
+
+  Bitmap vejrudsigt;
+
+  public class MinGrafik extends View {
+    float x, y;
+    float xFingerSidst, yFingerSidst;
+    public MinGrafik(Context context) {
+      super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+      Paint stregtype=new Paint();
+      canvas.drawBitmap(vejrudsigt, x, y, stregtype);
+      canvas.drawText("Dagens vejrudsigt:", 10, 5, stregtype);
+    }
+
+    @Override
+    public boolean onTrackballEvent(MotionEvent event) {
+      x = x + event.getX();
+      y = y + event.getY();
+      postInvalidate();
+      return true;
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+      System.out.println(event);
+      System.out.println(event.getAction());
+
+      if (event.getAction() == MotionEvent.ACTION_MOVE) {
+        // husk startpunkt
+        float dx = event.getX() - xFingerSidst;
+        float dy = event.getY() - yFingerSidst;
+        x = x + dx;
+        y = y + dy;
+        postInvalidate();
+      }
+      xFingerSidst = event.getX();
+      yFingerSidst = event.getY();
+      return true;
+    }
+  }
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    final TextView intro = new TextView(this);
+    intro.setText("Vent, indlæser vejrudsigt...");
+    intro.setTextSize(36);
+    setContentView(intro);
+
+    final MinGrafik minGrafik = new MinGrafik(this);
+
+
+    new AsyncTask() {
+
+      @Override
+      protected Object doInBackground(Object... arg) {
+        try {
+          int postnr = 2500;
+          vejrudsigt = ByvejrAktivitet.opretBitmapFraUrl("http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by=" + postnr + "&mode=long");
+          return "OK";
+        } catch (Exception ex) {
+          return "fejl: " + ex;
+        }
+      }
+
+      @Override
+      protected void onPostExecute(Object resultat) {
+        if ("OK".equals(resultat)) {
+          setContentView(minGrafik);
+        } else {
+          intro.append("\n\nBeklager, den ku ikke hentes\n\n" + resultat);
+        }
+      }
+    }.execute();
+
+  }
+}
