72e6ea6bebfb49e04c18fca6ec4f1cc0d4df523a
jacob.nordfalk <jacob.nordfalk@5618ce10-0a77-33da-ee7f-8326ecd09c32>
2011-09-22 13:38:24 +0000

    Ny kildekode

---
+++ trunk/AndroidElementer/assets/src/src/eks/levendeikon/VisKlokkenIkon.java
@@ -0,0 +1,82 @@
+package eks.levendeikon;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.util.Log;
+import android.widget.RemoteViews;
+import dk.nordfalk.android.elementer.R;
+import eks.grafik.Tegneprogram;
+import java.util.Arrays;
+import java.util.Date;
+
+
+public class VisKlokkenIkon extends AppWidgetProvider {
+  private static final String TAG = "MinAppwidgetProvider";
+
+  @Override
+  public void onUpdate(Context ctx, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
+
+    Log.d(TAG, "onUpdate "+Arrays.asList(appWidgetIds));
+    final RemoteViews remoteViews = new RemoteViews(ctx.getPackageName(), R.layout.levendeikon_visklokken);
+
+    remoteViews.setTextViewText(R.id.etTextView, "Klokken er:\n"+new Date());
+
+    // Vis en tilfældig farve på TextViewet
+    int farve = (int) System.currentTimeMillis() | 0xff0000ff;
+    remoteViews.setTextColor(R.id.etTextView, farve);
+
+    // generisk måde at gøre det samme på
+    remoteViews.setInt(R.id.etTextView, "setTextColor", farve);
+
+
+    // Lav et intent der skal affyres hvis knapppen trykkes
+    PendingIntent pendingIntent = PendingIntent.getActivity(ctx, 0,
+          new Intent(ctx, Tegneprogram.class), 0);
+
+    remoteViews.setOnClickPendingIntent(R.id.enKnap, pendingIntent);
+
+
+    appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+
+    // Lad uret opdatere hvert sekund i det næste minut. Burde gøres fra en service!!!
+    new Thread() {
+      public void run() {
+        for (int i=0; i<60; i++) {
+          try {
+            Thread.sleep(1000);
+          } catch (InterruptedException ex) {
+          }
+          remoteViews.setTextViewText(R.id.etTextView, "KL er:\n"+new Date());
+          remoteViews.setTextColor(R.id.etTextView, i%2==0? Color.RED : Color.GREEN);
+          appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+        }
+      }
+    }.start();
+
+  }
+
+  @Override
+  public void onReceive(Context c, Intent intent) {
+    super.onReceive(c, intent);
+    Log.d(TAG, "onReceive:" + intent);
+  }
+
+  @Override
+  public void onDeleted(Context context, int[] appWidgetIds) {
+    Log.d(TAG, "onDeleted");
+  }
+
+  @Override
+  public void onEnabled(Context context) {
+    Log.d(TAG, "onEnabled");
+  }
+
+  @Override
+  public void onDisabled(Context context) {
+    Log.d(TAG, "onDisabled");
+  }
+}
