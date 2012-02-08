ed146b1665242a969671ed3a85da9fb0c39d822f
jacob.nordfalk <jacob.nordfalk@5618ce10-0a77-33da-ee7f-8326ecd09c32>
2012-02-08 12:29:25 +0000

    Ny kildekode

---
+++ trunk/AndroidElementer/assets/src/eks/diverse/AfspilLyd.java
@@ -0,0 +1,38 @@
+package eks.diverse;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import dk.nordfalk.android.elementer.R;
+
+/**
+ *
+ * @author Jacob Nordfalk
+ */
+public class AfspilLyd extends Activity implements OnClickListener {
+
+	Button spilKnap;
+	MediaPlayer enLyd;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		spilKnap = new Button(this);
+		spilKnap.setText("Spil en lyd");
+		spilKnap.setOnClickListener(this);
+		setContentView(spilKnap);
+
+		// Volumen op/ned skal styre lydstyrken af medieafspilleren, uanset som noget spilles lige nu eller ej
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+		enLyd = MediaPlayer.create(this, R.raw.jeg_bremser_haardt);
+		enLyd.setVolume(1, 1);
+	}
+
+	public void onClick(View arg0) {
+		enLyd.start();
+	}
+}
