a4a200b5bcc2c380b101a33264e77ae3376ab846
jacob.nordfalk <jacob.nordfalk@5618ce10-0a77-33da-ee7f-8326ecd09c32>
2012-02-19 17:19:07 +0000

    skriftstørrelse kan ændres i visning af kildekode ekstern fremviser kan bruges til at se kildekode

--- trunk/AndroidElementer/src/dk/nordfalk/aktivitetsliste/VisKildekode.java
+++ trunk/AndroidElementer/src/dk/nordfalk/aktivitetsliste/VisKildekode.java
@@ -7,17 +7,26 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.text.util.Linkify;
 import android.text.util.Linkify.TransformFilter;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Level;
@@ -26,8 +35,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class VisKildekode extends Activity {
-
 	String filnavn;
+	TextView tv;
 	private static int onCreateTæller = 0;
 	public final static String KILDEKODE_FILNAVN = "filen der skal vises";
 	final static String LOKAL_PRÆFIX = "file:///android_asset/";
@@ -49,7 +58,12 @@ public class VisKildekode extends Activity {
 		super.onCreate(savedInstanceState);
 		findWebUrl(this);
 
-		TextView tv = new TextView(this);
+		tv = new TextView(this);
+
+		// Genskab foretrukken skriftstørrelse
+		float skriftstørrelse = getPreferences(MODE_PRIVATE).getFloat("skriftstørrelse", 0);
+		if (skriftstørrelse > 0) tv.setTextSize(TypedValue.COMPLEX_UNIT_PX, skriftstørrelse);
+
 		ScrollView sv = new ScrollView(this);
 		sv.addView(tv);
 		setContentView(sv);
@@ -74,7 +88,6 @@ public class VisKildekode extends Activity {
 
 
 				TransformFilter filter = new TransformFilter() {
-
 					public final String transformUrl(final Matcher match, String url) {
 						String klassenavn = match.group(1);
 						return "http://developer.android.com/reference/" + klassenavn.replace('.', '/') + ".html";
@@ -85,7 +98,6 @@ public class VisKildekode extends Activity {
 
 
 				filter = new TransformFilter() {
-
 					public final String transformUrl(final Matcher match, String url) {
 						// http://android.git.kernel.org/?p=platform/frameworks/base.git;a=blob;f=core/res/res/layout/simple_list_item_1.xml"
 						// http://android.git.kernel.org/?p=platform/frameworks/base.git;a=blob;f=core/res/res/" + match.group(1).replace('.', '/') + ".xml";
@@ -102,7 +114,6 @@ public class VisKildekode extends Activity {
 
 
 				filter = new TransformFilter() {
-
 					public final String transformUrl(final Matcher match, String url) {
 						// LOKAL_PRÆFIX dur ikke her da vi starter et webbrowserintent
 						return HS_PRÆFIX + "res/" + match.group(1).replace('.', '/') + ".xml";
@@ -114,7 +125,6 @@ public class VisKildekode extends Activity {
 
 				//
 				filter = new TransformFilter() {
-
 					public final String transformUrl(final Matcher match, String url) {
 						// LOKAL_PRÆFIX dur ikke her da vi starter et webbrowserintent
 						return HS_PRÆFIX + "src/" + match.group(1).replace('.', '/') + ".java";
@@ -127,7 +137,6 @@ public class VisKildekode extends Activity {
 
 				// Almindelig HTTP
 				filter = new TransformFilter() {
-
 					public final String transformUrl(final Matcher match, String url) {
 						return match.group(1);
 					}
@@ -161,10 +170,11 @@ public class VisKildekode extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0, 105, 0, "Skriftstørrelse...");
 		menu.add(0, 100, 0, "Vis i WebView");
 		menu.add(0, 102, 0, "Vælg fil");
 		menu.add(0, 103, 0, "Vis i browser");
-// XXX		menu.add(0, 104, 0, "Vis i andet...");
+		menu.add(0, 104, 0, "Vis i andet...");
 		return true;
 	}
 
@@ -177,10 +187,38 @@ public class VisKildekode extends Activity {
 		} else if (item.getItemId() == 103) {
 			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(HS_PRÆFIX + filnavn)));
 		} else if (item.getItemId() == 104) {
-//XXX			byte[] b = læsAssetFil(filnavn);
-//act=android.intent.action.VIEW dat=file:///mnt/sdcard/data/log.txt typ=text/plain flg=0x3 cmp=android/com.android.internal.app.ResolverActivity (has extras) }
-//			new Intent(Intent.ACTION_VIEW, Uri.parse(HS_PRÆFIX + filnavn);
-//			startActivity();
+			try {
+				byte[] b = læsAssetFil(filnavn);
+
+				File tmp = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "tmp");
+				tmp.mkdirs();
+				File f = new File(tmp, new File(filnavn).getName());
+				FileOutputStream fos = new FileOutputStream(f);
+				fos.write(b);
+				fos.close();
+				Intent i = new Intent(Intent.ACTION_VIEW, Uri.fromFile(f.getAbsoluteFile()));
+				i.setDataAndType(Uri.fromFile(f.getAbsoluteFile()), "text/plain");
+				//startActivity(Intent.createChooser(i, filnavn));
+				System.out.println("Gemmer fil i " + f);
+				System.out.println("Starter " + i.toURI());
+				startActivity(i);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				Toast.makeText(this, "Fejl : " + ex, Toast.LENGTH_LONG).show();
+			}
+		} else if (item.getItemId() == 105) {
+			final EditText input = new EditText(this);
+			input.setInputType(EditorInfo.TYPE_CLASS_NUMBER);
+			input.setText(String.valueOf((int) tv.getTextSize()));
+			new AlertDialog.Builder(this).setTitle("Vælg skriftstørrelse").
+					setView(input).
+					setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int whichButton) {
+					float str = Float.parseFloat(input.getText().toString());
+					getPreferences(MODE_PRIVATE).edit().putFloat("skriftstørrelse", str).commit();
+					tv.setTextSize(TypedValue.COMPLEX_UNIT_PX, str);
+				}
+			}).show();
 		}
 
 		return true;
@@ -191,7 +229,6 @@ public class VisKildekode extends Activity {
 			final String sti = filnavn.substring(0, filnavn.lastIndexOf("/"));;
 			final String[] filer = getAssets().list(sti);
 			new AlertDialog.Builder(this).setTitle("Filer i " + sti).setItems(filer, new Dialog.OnClickListener() {
-
 				public void onClick(DialogInterface arg0, int hvilken) {
 					filnavn = sti + "/" + filer[hvilken];
 					Toast.makeText(VisKildekode.this, "Viser " + filnavn, Toast.LENGTH_SHORT).show();
