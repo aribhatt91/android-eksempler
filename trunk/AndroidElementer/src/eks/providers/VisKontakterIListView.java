a9d38b763ead00ff67a044f86107fe7f0025513f
jacob.nordfalk <jacob.nordfalk@5618ce10-0a77-33da-ee7f-8326ecd09c32>
2012-03-13 10:40:06 +0000

    Korrekt brug af AsyncTask (der overlever skærmvending)

--- trunk/AndroidElementer/src/eks/providers/VisKontakterIListView.java
+++ trunk/AndroidElementer/src/eks/providers/VisKontakterIListView.java
@@ -1,5 +1,7 @@
 package eks.providers;
 
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.SimpleCursorAdapter;
 
 import android.app.Activity;
@@ -7,14 +9,18 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract.CommonDataKinds.Email;
 import android.provider.ContactsContract.Contacts;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 import dk.nordfalk.android.elementer.R;
 
 /**
  *
  * @author Jacob Nordfalk
  */
-public class VisKontakterIListView extends Activity {
+public class VisKontakterIListView extends Activity implements OnItemClickListener {
+
+	private SimpleCursorAdapter adapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -26,15 +32,24 @@ public class VisKontakterIListView extends Activity {
 				Contacts.IN_VISIBLE_GROUP + " = '1'", null, Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC");
 		startManagingCursor(cursor); // Lad cursoren følge aktivitetens livscyklus
 
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.listeelement, cursor,
+		adapter = new SimpleCursorAdapter(this, R.layout.listeelement, cursor,
 				// Disse kolonner i cursoren...
 				new String[]{Contacts.DISPLAY_NAME, Email.DATA},
 				// ... skal afbildes over i disse views i res/layout/listeelement.xml
-				new int[]{R.id.listeelem_overskrift, R.id.listeelem_beskrivelse});
+				new int[]{R.id.listeelem_overskrift, R.id.listeelem_beskrivelse}
+		);
 
 		ListView listView = new ListView(this);
 		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(this);
 
 		setContentView(listView);
+	}
+
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		Cursor cursor = adapter.getCursor();
+		cursor.moveToPosition(position);
+		String navn = cursor.getString(1); // Contacts.DISPLAY_NAME
+		Toast.makeText(this, navn, Toast.LENGTH_LONG).show();
 	}
 }
