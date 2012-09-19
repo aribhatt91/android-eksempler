8e383ae247e7ecc066fc04ec0b9a3c9f534dcc69
jacob.nordfalk <jacob.nordfalk@5618ce10-0a77-33da-ee7f-8326ecd09c32>
2012-09-19 19:35:10 +0000

    Ny kildekode

---
+++ trunk/AndroidElementer/assets/src/eks/grafik/Grafikdemo0.java
@@ -0,0 +1,47 @@
+package eks.grafik;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ *
+ * @author Jacob Nordfalk
+ */
+class GrafikView extends View {
+
+	// programmatisk konstruktør
+	public GrafikView(Context a) {
+		super(a);
+	}
+
+	// deklarariv konstruktør
+	public GrafikView(Context a, AttributeSet at) {
+		super(a, at);
+	}
+
+	@Override
+	protected void onDraw(Canvas c) {
+		Paint tekstStregtype = new Paint();
+		tekstStregtype.setColor(Color.GREEN);
+		tekstStregtype.setTextSize(24);
+		tekstStregtype.setAntiAlias(true);
+		c.drawText("Hej verden", 0, 20, tekstStregtype);
+	}
+}
+
+public class Grafikdemo0 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		GrafikView grafikView = new GrafikView(this);
+		setContentView(grafikView);
+	}
+}
